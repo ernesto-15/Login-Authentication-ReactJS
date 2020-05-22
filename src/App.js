6372f2550ea0247b7fe17cf93515a7bf8d674f6c
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-05-21 19:24:35 -0500

    Casi casi :'v

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PrivateRoute from './Components/PrivateRoute';
 import Home from './Pages/Home';
@@ -10,39 +10,45 @@ import Signup from './Pages/SignUp';
 function App(props) {
   const existingTokens = JSON.parse(localStorage.getItem('tokens'));
   const [authTokens, setAuthTokens] = useState(existingTokens);
-  const [isValidated, setIsValidated] = useState(false);
+  const [isValidated, setIsValidated] = useState(existingTokens);
+
+  const validateToken = async (token) => {
+    const resp = await fetch(
+      'https://puckllay-back.herokuapp.com/token/validation',
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    setIsValidated(resp.ok);
+    return resp.ok;
+    // validate(resp.ok);
+  };
+
   const setTokens = (data, logout) => {
-    if(logout) {
-      localStorage.removeItem('tokens');
-      setAuthTokens(null);  
-    }
-    else {
-      localStorage.setItem('tokens', JSON.stringify(data));
-      setAuthTokens(data);
+    const validated = validateToken(data);
+    if (validated) {
+      if (logout) {
+        localStorage.removeItem('tokens');
+        setAuthTokens(null);
+      } else {
+        localStorage.setItem('tokens', JSON.stringify(data));
+        setAuthTokens(data);
+      }
     }
   };
-  
-  const validate = (validated) => {
-    setIsValidated(validated)
-  }
 
-  // useEffect(() => {
-  //   console.log('object');
-  //   const fetchData = async () => {
-  //     const resp = await fetch('https://puckllay-back.herokuapp.com/token/validation', {
-  //       method: 'POST',
-  //       mode: 'cors',
-  //       headers: {
-  //         Authorization: `Bearer ${authTokens}`,
-  //       },
-  //     })
-  //     setIsValidated(resp.ok)
-  //   }
-  //   fetchData()
-  // }, []);
+  useEffect(() => {
+    validateToken(authTokens)
+  }, [authTokens]);
 
   return (
-    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens, validate, isValidated }}>
+    <AuthContext.Provider
+      value={{ authTokens, setAuthTokens: setTokens, isValidated }}
+    >
       <Router>
         <div>
           <ul>
--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../Context/auth';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const { authTokens, validate, isValidated } = useAuth();
-  // const [isValidated, setIsValidated] = useState(false)
-
-  useEffect(() => {
-    const validateToken = async () => {
-      const resp = await fetch(
-        'https://puckllay-back.herokuapp.com/token/validation',
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            Authorization: `Bearer ${authTokens}`,
-          },
-        }
-      );
-      validate(resp.ok);
-    };
-    validateToken();
-    // console.log(isValidated);
-  }, [authTokens, validate]);
+  const { isValidated } = useAuth();
   return (
     <Route
       {...rest}
