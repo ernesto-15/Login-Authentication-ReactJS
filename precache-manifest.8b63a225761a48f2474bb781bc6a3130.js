f0abfc5d290461e1aa55fdb1cfe9265c44bb2870
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-06-26 22:54:39 -0500

    Updates

---
+++ precache-manifest.8b63a225761a48f2474bb781bc6a3130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bee5976e17718759cfb001d7f395123",
+    "url": "/Login-Authentication-ReactJS/index.html"
+  },
+  {
+    "revision": "295d7c00f9294b56416e",
+    "url": "/Login-Authentication-ReactJS/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f3309e27010eaa565a4",
+    "url": "/Login-Authentication-ReactJS/static/js/2.13cb685c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Login-Authentication-ReactJS/static/js/2.13cb685c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295d7c00f9294b56416e",
+    "url": "/Login-Authentication-ReactJS/static/js/main.f3c615c6.chunk.js"
+  },
+  {
+    "revision": "883f2b6323b1ec2af4ea",
+    "url": "/Login-Authentication-ReactJS/static/js/runtime-main.c8a23095.js"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/Login-Authentication-ReactJS/static/media/logo.0e4fdce8.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Login-Authentication-ReactJS/precache-manifest.8b63a225761a48f2474bb781bc6a3130.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Login-Authentication-ReactJS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
